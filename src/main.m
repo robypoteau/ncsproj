841d2e71df86b4caf7bf2b7c95d433a49072c073
Roby <rpoteau2010@my.fit.edu>
2015-07-07 04:47:50 -0400

    sloppy paper

--- src/main.m
+++ src/main.m
@@ -5,13 +5,24 @@ pkg load odepkg
 %f = @(t,x)original(t,x,[150 2 3.2]);
 %[t,y] = ode45(f, 0:.1:20, x0);
 
-u = [150,2];
-params = [u 1];
-xd = [20 4];
-noise = 0;
-seed = 0;
-%x0 = [1,1,1];
+%%Controller when you know f
+%u = [150,2];
+%params = [145 2 2 1];
+%xd = [6 2];
+%noise = 3;
+%seed = 1;
+%x0 = [1,1,1,1];
 %f = @(t,x)known(t,x,u,params,xd,noise,seed);
+%[t,y] = ode45(f, 0:.1:20, x0);
+
+%Controller when you parameters vary
+u = [150,2];
+params = [150 2 15 1];
+xd = [6 2];
+noise = 3;
+seed = 1;
+x0 = [1 1];
+f = @(t,x)varyingparams(t,x,u,params,xd,noise,seed);
 [t,y] = ode45(f, 0:.1:20, x0);
 
 plot(t,y(:,1),'--g','LineWidth',2);
