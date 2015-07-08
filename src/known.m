8141da631f0faf91f17920267b8e9e36cfad0310
Roby <rpoteau2010@my.fit.edu>
2015-07-08 15:19:34 -0400

    added controller output stuff, adjusted paper

--- src/known.m
+++ src/known.m
@@ -1,6 +1,6 @@
 function dx = known(t,x,u,ctrlparam=[0 0 0 0.5], xd = [0 0], noise=0, seed=0)
      alpha = u(1);
-     n = u(2);
+     n = u(2); 
      
      %control laws
      ahat = ctrlparam(1);
@@ -9,8 +9,8 @@ function dx = known(t,x,u,ctrlparam=[0 0 0 0.5], xd = [0 0], noise=0, seed=0)
      nmax = noise*(.5);
      sa = x(1)-xd(1);
      sb = x(2)-xd(2);
-     u1 = -ahat/(1+x(2)^nhat) + x(1) - nmax - k*sat(sa,x(3));
-     u2 = -ahat/(1+x(1)^nhat) + x(2) - nmax - k*sat(sb,x(4));
+     u1 = -ahat/(1+x(2)^nhat) + x(1) - nmax - k*sat(sa,x(5));
+     u2 = -ahat/(1+x(1)^nhat) + x(2) - nmax - k*sat(sb,x(6));
      
      %noise
      m = noise;
@@ -22,6 +22,8 @@ function dx = known(t,x,u,ctrlparam=[0 0 0 0.5], xd = [0 0], noise=0, seed=0)
      l = ctrlparam(4);
      dx(1) = alpha/(1+x(2)^n) - x(1) + n1 + u1;
      dx(2) = alpha/(1+x(1)^n) - x(2) + n2 + u2;
-     dx(3) = -l*x(3) + .1;
-     dx(4) = -l*x(4) + .1;
+     dx(3) = u1;
+     dx(4) = u2;
+     dx(5) = -l*x(5) + .1;
+     dx(6) = -l*x(6) + .1;
 end
